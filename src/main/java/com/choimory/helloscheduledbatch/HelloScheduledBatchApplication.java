e67fc7b1a2f0c5dfdcd1c3c8f862b1c928c4ca15
YunjoongChoi <83948750+YunjoongChoi@users.noreply.github.com>
2021-06-22 17:33:00 +0900

    Initial commit

---
+++ src/main/java/com/choimory/helloscheduledbatch/HelloScheduledBatchApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.helloscheduledbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloScheduledBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloScheduledBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choimory/helloscheduledbatch/HelloScheduledBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.helloscheduledbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloScheduledBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
